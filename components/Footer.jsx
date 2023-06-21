156a5a99a84502d01740967d0fa3f354a3eec003
Hrithik-AJR <hrithikroy02001@gmail.com>
2023-06-21 22:53:40 +0530

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/layout';
+
+const Footer = () => (
+  <Box textAlign='center' p='5' color='.600' borderTop='4px' borderColor='yellow.500'>
+    © 2023 KingEstate, Inc.
+  </Box>
+);
+
+export default Footer;
